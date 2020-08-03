273b80f5a20586c5950033bba826fcbd9f43004d
Russel <emkil.russel@gmail.com>
2020-08-03 10:16:18 +0300

    animation extended

--- SoraUI/Classes/Animator/TransitionAnimator.swift
+++ SoraUI/Classes/Animator/TransitionAnimator.swift
@@ -8,10 +8,17 @@ import UIKit
 public final class TransitionAnimator: ViewAnimatorProtocol {
     public var duration: TimeInterval
     public var type: CATransitionType
+    public var subtype: CATransitionSubtype?
+    public var curve: CAMediaTimingFunctionName?
 
-    public init(type: CATransitionType, duration: TimeInterval = 0.25) {
+    public init(type: CATransitionType,
+                duration: TimeInterval = 0.25,
+                subtype: CATransitionSubtype? = nil,
+                curve: CAMediaTimingFunctionName? = nil) {
         self.type = type
         self.duration = duration
+        self.subtype = subtype
+        self.curve = curve
     }
 
     public func animate(view: UIView, completionBlock: ((Bool) -> Void)?) {
@@ -20,6 +27,12 @@ public final class TransitionAnimator: ViewAnimatorProtocol {
         let animation = CATransition()
         animation.type = type
         animation.duration = duration
+        animation.subtype = subtype
+
+        if let curve = curve {
+            animation.timingFunction = CAMediaTimingFunction(name: curve)
+        }
+
         view.layer.add(animation, forKey: nil)
 
         if let completion = completionBlock {
